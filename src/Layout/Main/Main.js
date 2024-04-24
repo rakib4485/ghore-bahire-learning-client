d3fc062e546e63ef84c5e7acbac27b8e2f70cbd3
xbayazid <bayazid9006@gmail.com>
2024-04-24 13:15:22 +0600

    main layout added

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
-            
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
         </div>
     );
 };
